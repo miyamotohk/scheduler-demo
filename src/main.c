7db86da9cedaf8249b4739f25f97c328f40e8c46
Henrique Miyamoto <miyamotohk@gmail.com>
2017-10-02 18:00:31 -0300

    finalizado

--- src/main.c
+++ src/main.c
@@ -3,11 +3,22 @@
 int main() {
   int a[5];
   int n;
+  int flag;
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
-
-  for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
+  // A cada iteracao do scheduler
+  for (int i=1; i<n+1; i++) {
+  	flag = 0;
+  	// Verifica cada temporizador
+  	for (int j=0; j<5; j++){
+  		if(i%a[j] == 0){
+  			printf("%d",j);
+  			flag = 1;
+  		}
+  	}
+  	if(flag == 0)
+  		printf("-");
+  	printf("\n");
   }
 
   return 0;
